809f8dbe35f9243bd33ce8c61f1ddbe62fd7164b
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-15 17:32:59 +0100

    Adds methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  def error_formatter(resource, status = :unprocessable_entity)
+    render json: {
+      error: {
+        title: "une erreur s'est produite",
+        message: resource.errors.full_messages.join('; ')
+      }
+    }, status: status
+  end
+
+  def render_user_json
+    # avatar = if current_user.avatar.attached?
+    #            rails_blob_url(current_user.avatar)
+    #          end
+    render json: {
+      id: current_user.id,
+      email: current_user.email,
+      username: current_user.username || nil,
+      # avatar: avatar
+    }, status: :ok
+  end
 end
