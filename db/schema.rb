c0027db4e6f1f76b62424d2005d8fa4345bada9b
talmidiel <talmidiel.officiel@gmail.com>
2022-03-15 13:21:24 +0100

    fixed the seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_091635) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_15_102218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_091635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "snippets", force: :cascade do |t|
+    t.text "content"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_snippets_on_post_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,22 @@
 User.create(email: 'anthony@exemple.com', password: 'password')
 
 30.times do
-  snippet = Snippet.create(content: "const averageBy = (arr, fn) =>
-  arr
-    .map(typeof fn === 'function' ? fn : val => val[fn])
-    .reduce((acc, val) => acc + val, 0) / arr.length;
-
-averageBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], o => o.n); // 5
-averageBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], 'n'); // 5")
-  Post.create(
+  post = Post.create(
     user: User.first,
     description: "Calculates the average of an array, after mapping each element to a value using the provided function.
 
     Use Array.prototype.map() to map each element to the value returned by fn.
     Use Array.prototype.reduce() to add each value to an accumulator, initialized with a value of 0.
     Divide the resulting array by its length.",
-    snippet: snippet
+  )
+  Snippet.create(
+    content: "const averageBy = (arr, fn) =>
+  arr
+    .map(typeof fn === 'function' ? fn : val => val[fn])
+    .reduce((acc, val) => acc + val, 0) / arr.length;
+
+averageBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], o => o.n); // 5
+averageBy([{ n: 4 }, { n: 2 }, { n: 8 }, { n: 6 }], 'n'); // 5",
+    post: post
   )
 end
