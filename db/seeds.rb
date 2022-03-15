000942ad3667900974993d3bd6c3e90ac07040d6
talmidiel <talmidiel.officiel@gmail.com>
2022-03-15 11:05:30 +0100

    fixed a typo in migration and added a small seed for tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'anthony@exemple.com', password: 'password')
+
+30.times do
+  Post.create(
+    user: User.first,
+    description: 'HTML Pro Tip 💡
+
+Use "translate" attribute and set it value to "no" for your company name.
+
+So that in case, the webpage is translated into another language, your brand name will remain intact.'
+  )
+end
