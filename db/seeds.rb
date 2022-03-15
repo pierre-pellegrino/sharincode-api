f4b6c5a61db917b95bd3b6f63fcbae6ac8aa14da
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-15 14:48:33 +0100

    adds basic seed 4 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+
+def loading_bar(i, total, action)
+  count = i * 50 / total
+  system('clear')
+  puts action
+  puts "#{i * 100 / total}% [#{'#' * count}#{' ' * (50 - count)}]"
+end
+
+loading_bar(1, 4, 'creating users')
+user1 = User.create(
+  email: 'b@yopmail.com',
+  password: "password",
+)
+# user1.avatar.attach(
+#   io: File.open(Rails.root.join('app', 'assets', "avatar1.jpg")),
+#   filename: 'avatar.jpg'
+# )
+
+loading_bar(2, 4, 'creating users')
+user2 = User.create(
+  email: 'p@yopmail.com',
+  password: "password",
+)
+# user2.avatar.attach(
+#   io: File.open(Rails.root.join('app', 'assets', "avatar2.jpg")),
+#   filename: 'avatar.jpg'
+# )
+
+loading_bar(3, 4, 'creating users')
+user3 = User.create(
+  email: 't@yopmail.com',
+  password: "password",
+)
+# user3.avatar.attach(
+#   io: File.open(Rails.root.join('app', 'assets', "avatar3.jpg")),
+#   filename: 'avatar.jpg'
+# )
+
+loading_bar(4, 4, 'creating users')
+user4 = User.create(
+  email: 'a@yopmail.com',
+  password: "password",
+)
+# user4.avatar.attach(
+#   io: File.open(Rails.root.join('app', 'assets', "avatar4.jpg")),
+#   filename: 'avatar.jpg'
+# )
