acac7fcfd794f4b95191ab9a829eb20b5b1f4602
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-17 20:30:04 +0100

    adds associations to models Post, Tag, and PostTag

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :snippets
   has_many :comments
+  has_one :post_tag, dependent: :destroy
+  has_many :tags, through: :post_tag
 
   validates :description, length: { maximum: 500 }
 end
