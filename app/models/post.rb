9660c5f6da07e5f285609d9b2f5cc7abc766d002
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-17 20:38:49 +0100

    corrects associations post and posttag

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :snippets
   has_many :comments
-  has_one :post_tag, dependent: :destroy
-  has_many :tags, through: :post_tag
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
 
   validates :description, length: { maximum: 500 }
 end
