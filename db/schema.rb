53d59ae085f9e1e76c1fd5352f66942e2c7d6dcb
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-17 17:30:25 +0100

    migrated to adds attributes to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_17_135225) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_17_162944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_17_135225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.text "description"
+    t.string "github_url"
+    t.string "personal_url"
+    t.string "favorite_theme"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
