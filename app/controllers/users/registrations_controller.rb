711808c341e04e972077e07b6b5d50c83170a217
talmidiel <talmidiel.officiel@gmail.com>
2022-03-21 14:15:34 +0100

    cleaned the code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,19 +2,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   private
-  
+
   def respond_with(resource, _opts = {})
     register_success && return if resource.persisted?
     error_formatter(resource, :unauthorized)
   end
-  
+
   def register_success
+    UserMailer.welcome_email(current_user).deliver!
     message = 'New user sucessfully registered !'
     render_user(message)
   end
-  
+
   def sign_up_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+
 end
