4089ff7380624d71be362253fc15d3bd6a0d992d
talmidiel <talmidiel.officiel@gmail.com>
2022-03-21 12:32:48 +0100

    trying with the cnfig directly in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'snipshare.vercel.app'}
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:mailjet, :public_key),
+    password: Rails.application.credentials.dig(:mailjet, :secret_key),
+    domain: 'snipshare.dev@outlook.com',
+    address: 'in-v3.mailjet.com',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
