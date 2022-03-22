d0a621a056f2abf4e67efd45eb602f66566decf8
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-22 15:48:24 +0100

    cleans routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users,
-  controllers: {
+  devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  # Need to move index method in resource instead of resources (check with Boris first) or create path /me
   resources :profiles, only: %i[index show update] 
   resource :profiles, only: %i[destroy]
   resources :posts, only: %i[index show update create destroy] do
