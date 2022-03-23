0ef042b9445eb95ee4304d2d0d9f363ed990a0a7
talmidiel <talmidiel.officiel@gmail.com>
2022-03-23 17:04:14 +0100

    added a request to get private emails if user email is not publicly visible

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,8 @@ class AuthController < ApplicationController
 
   def index
     get_user_data
+    get_private_email if @user_data['email'].nil?
+
     @user = User.from_github(@user_data)
     jwt = Warden::JWTAuth::UserEncoder.new.call(@user, :user, nil)
 
@@ -26,14 +28,16 @@ class AuthController < ApplicationController
     response = HTTParty.post(url, {
                                headers: headers
                              })
-    response.parsed_response['access_token']
+    @access_token = response.parsed_response['access_token']
   end
 
   def get_user_data
     url = 'https://api.github.com/user'
-    access_token = get_access_token
+
+    get_access_token
+
     headers = {
-      Authorization: "token #{access_token}",
+      Authorization: "token #{@access_token}",
       Accept: 'application/json'
     }
 
@@ -41,5 +45,18 @@ class AuthController < ApplicationController
                               headers: headers
                             })
     @user_data = response.parsed_response
+  end
+
+  def get_private_email
+    url = 'https://api.github.com/user/emails'
+    headers = {
+      Authorization: "token #{@access_token}",
+      Accept: 'application/json'
+    }
+
+    response = HTTParty.get(url, {
+                              headers: headers
+                            })
+    @user_data['email'] = response.parsed_response[0]['email']
   end
 end
