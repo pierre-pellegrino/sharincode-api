99db4ac80c96bf3bdd696666ebd288554f8c45f0
talmidiel <talmidiel.officiel@gmail.com>
2022-03-24 12:53:34 +0100

    added controller logic for create and destroy

--- app/controllers/favorite_posts_controller.rb
+++ app/controllers/favorite_posts_controller.rb
@@ -1,5 +1,36 @@
 class FavoritePostsController < ApplicationController
-  def create; end
+  before_action :authenticate_user!
 
-  def destroy; end
+  def create
+    post = FavoritePost.new(
+      post: Post.find(params[:post_id]),
+      user: current_user
+    )
+    if post.save
+      render json: {
+        message: 'post added to favorites'
+      }
+    else
+      error_formatter(post)
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if current_user.id != @post.user.id
+      render json: {
+        error: {
+          title: "can't remove favorite posts from other users"
+        }
+      }
+      return
+    end
+
+    @post.destroy
+
+    render json: {
+      message: 'post removed from favorites'
+    }
+  end
 end
