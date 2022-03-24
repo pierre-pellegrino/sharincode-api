7e25ff272e90c359f1d9a08dfcb8fc01441e00a9
talmidiel <talmidiel.officiel@gmail.com>
2022-03-24 12:56:47 +0100

    updated models to add posts associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :snippets, through: :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :favorite_posts, dependent: :destroy
   has_one_attached :avatar, dependent: :destroy
 
   validates :username,
