5ac95f5ef7973243a78c3a7c9c037bf3674cac27
talmidiel <talmidiel.officiel@gmail.com>
2022-03-24 12:40:32 +0100

    added table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_23_120310) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_24_113138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_120310) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorite_posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_favorite_posts_on_post_id"
+    t.index ["user_id"], name: "index_favorite_posts_on_user_id"
   end
 
   create_table "jwt_denylist", force: :cascade do |t|
