fa292c4d9b030d7bc438d2d02eb173ab77c26950
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-26 18:41:07 +0100

    corrects factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,24 +1,26 @@
-# FactoryBot.define do
-#   factory :comments, class: Comment do
-#     text { Faker::Lorem.paragraph }
-#     post { FactoryBot.create(:post) }
-#     user { FactoryBot.create(:user) }
-#   end
+FactoryBot.define do
 
-#   factory :user, class: User do
-#     email { "rspec_tests@yopmail.com" }
-#     password { "password" }
-#     username { "rspec_test" }
-#   end
+  factory :user, class: User do
+    email { "rspec_tests@yopmail.com" }
+    password { "password" }
+    username { "rspec_test" }
+  end
 
-#   factory :random_user, class: User do
-#     email { Faker::Internet.email }
-#     password { "password" }
-#     username { Faker::Lorem.word }
-#   end
+  factory :random_user, class: User do
+    email { Faker::Internet.email }
+    password { 'password' }
+    username { "base #{Faker::Lorem.word}" }
+  end
 
-#   factory :post, class: Post do
-#     description { Faker::Lorem.paragraph }
-#     user { FactoryBot.create(:random_user) }
-#   end
-# end
+  factory :post, class: Post do
+    description { Faker::Lorem.paragraph }
+    user { FactoryBot.create(:random_user) }
+  end
+
+  factory :comments, class: Comment do
+    text { Faker::Lorem.paragraph }
+    post { FactoryBot.create(:post) }
+    user { FactoryBot.create(:user) }
+  end
+
+end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -6,18 +6,34 @@ RSpec.describe User, { type: :model } do
       id: 11,
       username: 'tester',
       email: 'test@test.com',
-      password: 'azerty'
+      password: 'azerty',
+      description: 'Description test',
+      github_url: 'github@github.com',
+      personal_url: 'personal@yopmail.com',
+      favorite_theme: 'myTheme'
     )
   }
-  it "User is valid with valid attributes" do
+  it 'User is valid with valid attributes' do
     expect(subject).to be_valid
   end
-  it "User is not valid without an email" do
-    subject.email = nil
+  it 'User is not valid without a username' do
+    subject.username = nil
     expect(subject).to_not be_valid
   end
-  it "Password should be at least 6 caracters" do
-    subject.password = "bla"
+  it 'Username should be at least 4 chars' do
+    subject.username = 'Bla'
+    expect(subject).to_not be_valid
+    subject.username = 'Blabla'
+    expect(subject).to be_valid
+  end
+  it 'User is not valid without a valid formated email' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+    subject.email = 'yopmail.com'
+    expect(subject).to_not be_valid
+  end
+  it 'Password should be at least 6 caracters' do
+    subject.password = 'bla'
     expect(subject).to_not be_valid
   end
 end
