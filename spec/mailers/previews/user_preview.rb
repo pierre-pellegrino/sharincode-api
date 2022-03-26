6f71acf13ecbc6d63b2fb0971516152247911a76
TimotheeGimbert <timothee.gimbert@free.fr>
2022-03-26 17:40:48 +0100

    commenting inactive tests

--- spec/mailers/previews/user_preview.rb
+++ spec/mailers/previews/user_preview.rb
@@ -1,4 +1,4 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user
-class UserPreview < ActionMailer::Preview
+# # Preview all emails at http://localhost:3000/rails/mailers/user
+# class UserPreview < ActionMailer::Preview
 
-end
+# end
